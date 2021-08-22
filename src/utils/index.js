73c443c19acba152db57120b9cfaf9306f3a64c4
luoxuzhi <herolxz@163.com>
2021-08-22 22:09:11 +0800

    feat(utils/index.js): 区分开发生产环境下的ws地址

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,5 @@
 // https://www.cnblogs.com/mafly/p/websocket.html
-export const ws = new WebSocket('wss://v2.ncuxz.fun/websocket')
+
+export const ws = new WebSocket(
+  process.env.NODE_ENV === 'production' ? 'wss://v2.ncuxz.fun/websocket' : 'ws://localhost:4000/websocket'
+)
