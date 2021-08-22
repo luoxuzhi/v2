102d7b10fe4a54ae4fd5e08e140fafc3f2aad0a6
luoxuzhi <herolxz@163.com>
2021-08-22 22:46:31 +0800

    feat(wss/index.vue): 修改wss demo页面

--- src/components/wss/index.vue
+++ src/components/wss/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="ruleone-wrapper">
-    <div @click="handleClick">click me to request server</div>
-    <div>serverResponse:{{ serverResponse }}</div>
-    <div>websocketMsg:{{ websocketMsg }}</div>
+  <div class="ruleone-wrapper" v-loading="loading">
+    <div class="handlediv item" @click="handleClick">点击模拟文件上传，后台3s后处理完成并推送前端结果</div>
+    <div class="item">serverResponse: {{ serverResponse }}</div>
+    <div class="item">websocketMsg: {{ websocketMsg }}</div>
   </div>
 </template>
 
@@ -13,11 +13,14 @@ export default {
   data() {
     return {
       serverResponse: '',
-      websocketMsg: ''
+      websocketMsg: '',
+      loading:false
     }
   },
   methods: {
     handleClick() {
+      this.loading=true
+        this.$message('模拟文件上传，3s后上传完成')
       axios.get('/wss').then(res => {
         this.serverResponse = res.data
       })
@@ -25,10 +28,20 @@ export default {
   },
   mounted() {
     ws.onmessage = event => {
-      this.websocketMsg = event.data
+      const {msg,isCompleted} = JSON.parse(event.data)
+      this.websocketMsg = msg
+      this.loading=false
+      if(isCompleted) this.$message.success('文件上传成功')
     }
   }
 }
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus"></style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+.handlediv{
+  cursor pointer
+}
+.item{
+  line-height 40px
+}
+</style>
