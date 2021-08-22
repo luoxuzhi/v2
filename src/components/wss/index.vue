10a12bcb2194114af009e15e9f659902d10e2fee
luoxuzhi <herolxz@163.com>
2021-08-22 23:15:43 +0800

    feat(wss/index.vue): add example

--- src/components/wss/index.vue
+++ src/components/wss/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ruleone-wrapper" v-loading="loading">
     <div class="handlediv item" @click="handleClick">点击模拟文件上传，后台3s后处理完成并推送前端结果</div>
+    <div class="handlediv item" @click="handleAdd">初始数字是: {{ num }}，点击数字自增，如果打开了其它浏览器窗口会同步更新</div>
     <div class="item">serverResponse: {{ serverResponse }}</div>
     <div class="item">websocketMsg: {{ websocketMsg }}</div>
   </div>
@@ -14,7 +15,8 @@ export default {
     return {
       serverResponse: '',
       websocketMsg: '',
-      loading:false
+      loading:false,
+      num:99
     }
   },
   methods: {
@@ -24,14 +26,18 @@ export default {
       axios.get('/wss').then(res => {
         this.serverResponse = res.data
       })
+    },
+    handleAdd(){
+      axios.get('/wss/add',{params:{num:this.num+1}})
     }
   },
   mounted() {
     ws.onmessage = event => {
-      const {msg,isCompleted} = JSON.parse(event.data)
+      const {msg,isCompleted,num} = JSON.parse(event.data)
       this.websocketMsg = msg
       this.loading=false
       if(isCompleted) this.$message.success('文件上传成功')
+      if(num) this.num += 1
     }
   }
 }
