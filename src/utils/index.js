5158c972ca169d48dbfa0eedd0276f3a0a8b14f6
luoxuzhi <herolxz@163.com>
2021-08-22 21:50:07 +0800

    feat(xx): xx

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,2 @@
 // https://www.cnblogs.com/mafly/p/websocket.html
-export const ws = new WebSocket('wss://server.ncuxz.fun/websocket')
+export const ws = new WebSocket('wss://v2.ncuxz.fun/websocket')
